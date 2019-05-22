77ec2fadfde24d2b580a8896bcdca1e99955a942
29bucuk <pinargultekin@yahoo.com>
2019-05-22 14:57:29 -0400

    Variables created

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+//Variables
+var correct= 0;
+var incorrect= 0;
+var unanswered= 0;
+var timer= 25;
+
+var questions= ["What was wrong with the couch Ross returned to the store?",
+                "What was Phoebe in charge of at Rachel's suprise party?",
+                "What is the name of Phoebe's alter-ego?",
+                "Who was Chandler's TV magazine always addressed to?",
+                "Which is not a Phoebe song?",
+                "What was Monica's apartment number?",
+                "What is Chandler's address in Yemen?",
+                "What is the name of Chandler's roommate after Joey moves out?"]
+
+var options= [ ["It had stain","It was torn","It was cut in half", "The color was wrong"],
+                ["Ice and food", "Cups and food","Cups and ice", "Balloons and ice"],
+                ["Phoebe Neeby", "Regina Falange", "Princess Consuela Bananahammock", "Mrs.Crap Bag"],
+                ["Chandler Bang", "Chandler Bung", "Miss Chanandler Bong", "Chandler Beng"],
+                ["Double-jointed boy", "My mother's ashes", "Terry's a jerk", "My mom, the cat"],
+                ["16","24", "20", "19"],
+                ["15 Yemen Road", "15 Yemen Street", "15 Yemen Avenue", "15 Yemen Drive"],
+                ["George", "David", "Eddie", "Frank"]]
+
+var answers = ["It was cut in half", "Cups and ice", "Regina Falange", "Miss Chanandler Bong", "My mom, the cat",
+                "20", "15 Yemen Road", "Eddie" ]
+//Functions
+function startGame() {
+
+}
+
+//Reset Game
