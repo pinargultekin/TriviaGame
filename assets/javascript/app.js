ae6cb6790385fce2d1bee8b12cd43229d16911bd
29bucuk <pinargultekin@yahoo.com>
2019-05-29 23:05:17 -0400

    timer & start function added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,87 +1,58 @@
-//Variables
-var questions = [{
-  question: "What was wrong with the couch Ross returned to the store?",
-  options: ["It had stain", "It was torn", "It was cut in half", "The color was wrong"],
-  correctAnswer: "It was cut in half"
-}, {
-  question: "What was Phoebe in charge of at Rachel's suprise party?",
-  options: ["Ice and food", "Cups and food", "Cups and ice", "Balloons and ice"],
-  correctAnswer: "Cups and ice"
-}, {
-  question: "What is the name of Phoebe's alter-ego?",
-  options: ["Phoebe Neeby", "Regina Falange", "Princess Consuela Bananahammock", "Mrs.Crap Bag"],
-  correctAnswer: "Regina Falange"
-}, {
-  question: "Who was Chandler's TV magazine always addressed to?",
-  options: ["Mr. Chandler Bang", "Chandler Bung", "Miss Chanandler Bong", "Chandler Beng"],
-  correctAnswer: "Miss Chanandler Bong"
-}, {
-  question: "Which is not a Phoebe song?",
-  options: ["Double-jointed boy", "My mother's ashes", "Terry's a jerk", "My mom, the cat"],
-  correctAnswer: "My mom, the cat"
-}, {
-  question: "What was Monica's apartment number?",
-  options: ["16", "24", "20", "19"],
-  correctAnswer: "20"
-}, {
-  question: "What is Chandler's address in Yemen?",
-  options: ["15 Yemen Road", "15 Yemen Street", "15 Yemen Avenue", "15 Yemen Drive"],
-  correctAnswer: "15 Yemen Road"
-}, {
-  question: "What is the name of Chandler's roommate after Joey moves out?",
-  options: ["George", "David", "Eddie", "Frank"],
-  correctAnswer: "Eddie"
-}];
+$(document).ready(function () {
 
-var currentQuestion = 0;
-var correct = 0;
-var incorrect = 0;
-var blank = 0;
-var timer = 25;
-var isTimer = false;
-var set = 0;
-var userChoice = "";
+  $("#results").hide();
+  $("#game").hide();
 
-//Function
-//When start button clicked;
-$("#start").on("click", function () {
-  //start button dissapear
-  $("#start").remove();
-  //and game card appears 
-  $("#game").show();
-  //with questions and options
-  ask();
-  //timer starts count down
-  startTimer();
+  $("#start").on("click", function () {
+    //start button dissapear
+    $("#start").remove();
+    //and game card appears 
+    $("#game").show();
+    //timer starts count down
+    startGame.startTimer();
+    //changing background color when time is started 
+    $("#counting").css("background-color", "rgb(237, 237, 90)");
+  });
 });
 
-//Loading questions and options
-function ask() {
- //question call
- $("#questions").html("<h3>" + questions[currentQuestion].question + "</h3>");
 
- //options call
- for (var i = 0; i < questions[currentQuestion].options.length; i++) {
-   $("#options").append('<label><input type="radio" name="option" class="choices" ' + i + ' "value="'
-     + questions[currentQuestion].options[i] + '  ">' + " " + questions[currentQuestion].options[i] + '</label>');
- }
- // if (userChoice !== null) {
- //   clicked();
- // }
-};
 
-//Timer
-function startTimer() {
-  var timer = 25;
-  var interval = setInterval(function () {
- 
-    document.getElementById("display-time").innerHTML = timer;
-    timer--;
-    if (timer === 0) {
-      clearInterval(interval);
-      document.getElementById("display-time").innerHTML = timer;
-      document.getElementById("game").innerHTML = askNext;
+var startGame = {
+
+  timer: 120,
+
+  // start the timer & show the questions
+  startTimer: function () {
+    $("#display-time").text("Time remaining: " + startGame.timer);
+    setInterval(startGame.countdown, 1000);
+    trivia.displayQuestions();
+  },
+
+  // decrement the timer
+  countdown: function () {
+    startGame.timer--;
+    $("#display-time").text("Time remaining: " + startGame.timer);
+    if (startGame.timer === 0) {
+      startGame.stopTimer();
+      $("#display-time").empty();
     }
-  }, 1000);
- };
- 
+  },
+
+  // stop the timer and check the options
+  stopTimer: function () {
+    clearInterval();
+    trivia.checkAnswers();
+  },
+
+  // hide the questions and time & display the result page
+  showResultPage: function (numCorrect, numIncorrect, numUnanswered) {
+
+    $("#results").show();
+    $("#questions").empty();
+    $("#display-time").empty();
+    $("#counting").remove();
+    $("#correct-options").text("Correct: " + numCorrect);
+    $("#incorrect-options").text("Incorrect: " + numIncorrect);
+    $("#unanswered").text("Unanswered: " + numUnanswered);
+  }
+};
